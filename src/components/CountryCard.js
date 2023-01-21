155abfa4f6f6534a512e3a3e7088fac756ab988d
akanksha.pradhan <akanksha.pradhan@Airdeakapradhan.lan>
2023-01-21 23:10:14 +0100

    hover effect done

--- src/components/CountryCard.js
+++ src/components/CountryCard.js
@@ -1,8 +1,5 @@
 import React from "react";
 import Records from "../components/records.json";
-// import GridList from "@material-ui/core/GridList";
-// import GridListTile from "@material-ui/core/GridListTile";
-// import GridListTileBar from "@material-ui/core/GridListTileBar";
 import "./countryCard.css";
 import { Container, Row, Col } from "react-bootstrap";
 
@@ -26,6 +23,10 @@ function CountryCard() {
                 <div className="country-title">
                   <h5>{record.country}</h5>
                   <h3>{record.city}</h3>
+                  <p className="country-detail">{record.details}</p>
+                  <a href="#" className="country-detail explore-button">
+                    Explore More
+                  </a>
                 </div>
               </div>
             </Col>
